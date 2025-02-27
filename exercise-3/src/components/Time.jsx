07b9dfa0eb2483583ff78c11d6bab28f5b35ead3
araeht <meakputsoktheara13@gmail.com>
2025-02-27 10:37:10 +0700

    final commit

--- exercise-3/src/components/Time.jsx
+++ exercise-3/src/components/Time.jsx
@@ -1,10 +1,13 @@
 function Time() {
-    let date = new Date().toLocaleString() + ""
-    return(
-        <h2>
-            {date}
-        </h2>
-    );
+  const now = new Date();
+  const time = now.toLocaleTimeString(); // Extracts only time
+
+  return (
+    <div>
+      <h2>Current Time:</h2>
+      <p>{time}</p>
+    </div>
+  );
 }
 
 export default Time;
--- exercise-4/src/App.jsx
+++ exercise-4/src/App.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
 function App() {
-  /* Contants used in this component */
+  /* Constants used in this component */
   const DOLLAR_TO_EURO_RATIO = 0.92;
   const DOLLAR_TO_DONG_RATIO = 24560;
 
   let valueDollars = 15;
 
-  // TODO : You need to implement and call this function in your JSX code
   // Convert the given value in dollars to a value in euro
-  function dollarToEuro(valueInDollars) {}
+  function dollarToEuro(valueInDollars) {
+    return valueInDollars * DOLLAR_TO_EURO_RATIO;
+  }
 
-  // TODO : You need to implement and call this function in your JSX code
   // Convert the given value in dollars to a value in dong
-  function dollarToDong(valueInDollars) {}
+  function dollarToDong(valueInDollars) {
+    return valueInDollars * DOLLAR_TO_DONG_RATIO;
+  }
 
   return (
     <main>
@@ -23,13 +25,11 @@ function App() {
         <label>Current value in dollars</label>
         <input disabled value={valueDollars} />
 
-        {/* TODO This input need to display the value in dongs */}
         <label>Value in Dong</label>
-        <input disabled />
+        <input disabled value={dollarToDong(valueDollars)} />
 
-        {/* TODO This input need to display the value in euros */}
         <label>Value in Euro</label>
-        <input disabled />
+        <input disabled value={dollarToEuro(valueDollars)} />
       </p>
     </main>
   );
